626377f34032e58f35df9ca0356865cbfe6fbdb8
Borys Mch <borys.mch@gmail.com>
2026-05-12 16:35:09 +0300

    Add Zen browsew

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }:
--- modules/packages.nix
+++ modules/packages.nix
@@ -6,6 +6,7 @@
 
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     wget
     fastfetch
     appimage-run
