a0798c7a3724b7bc1395f48884c928a4cd161f10
Borys Mch <borys.mch@gmail.com>
2026-05-14 15:44:30 +0300

    Add zsh

--- modules/users.nix
+++ modules/users.nix
@@ -5,6 +5,7 @@
     isNormalUser = true;
     description = "Borys Mch";
     extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" "kvm" "dialout" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
     #  thunderbird
     ];
