c1fa54aba6cb25117226716357d15b40629510dc
Borys Mch <borys.mch@gmail.com>
2026-05-20 12:33:10 +0300

    Update

--- modules/packages.nix
+++ modules/packages.nix
@@ -8,6 +8,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     wget
+    python3
     fastfetch
     appimage-run
     mesa-demos
