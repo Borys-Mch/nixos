303e2df097bd9713c7230580b2e9d4deee42e446
Borys Mch <borys.mch@gmail.com>
2026-06-06 10:16:47 +0300

    Fix OrcaSlicer networking

--- modules/system.nix
+++ modules/system.nix
@@ -48,6 +48,13 @@
   # Enable networking
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.powersave = false;
+  networking.firewall = {
+    extraCommands = ''
+      iptables -I INPUT -m pkttype --pkt-type multicast -j ACCEPT
+      iptables -A INPUT -m pkttype --pkt-type multicast -j ACCEPT
+      iptables -I INPUT -p udp -m udp --match multiport --dports 1990,2021 -j ACCEPT
+    '';
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Kyiv";
