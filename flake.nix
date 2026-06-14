848363d2da6ae97f47b8b9852d30420683167bd6
Borys Mch <borys.mch@gmail.com>
2026-06-14 13:05:48 +0300

    Pinned the release

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser = {
