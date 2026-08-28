08ca15b32a76c98522f2211ea6fdbe4a73149911
Borys Mch <borys.mch@gmail.com>
2026-08-28 20:51:19 +0300

    Update

--- modules/system.nix
+++ modules/system.nix
@@ -24,7 +24,7 @@
     
     # Pinned to 7.1 because NVIDIA 595.71.05 fails to build on Linux 7.2
     # (missing <string.h> include in nvidia/os-interface.c).
-    kernelPackages = pkgs.linuxPackages_7_1;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [
       "quiet"
       "udev.log_level=3"
